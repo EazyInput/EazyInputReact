3e08004e37a24b2400dbf91a8ecafc11b53bbf03
Michael B <mjbradvica@gmail.com>
2024-12-02 17:06:49 -0500

    fix build errors

--- src/library/hooks/use-date-input.ts
+++ src/library/hooks/use-date-input.ts
@@ -7,12 +7,17 @@ export default function useDateInput(
   return useInput<Date, string>(initialValue, (updated) => {
     const parts = updated.split("-");
 
+    const year = Number.parseInt(
+      parts.at(0) ?? new Date().getFullYear().toString(),
+    );
+
+    const month =
+      Number.parseInt(parts.at(1) ?? new Date().getMonth().toString()) - 1;
+
+    const day = Number.parseInt(parts.at(2) ?? new Date().getDay().toString());
+
     // A normal parse will produce UTC, resulting in a off by one day error.
     // new Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
-    return new Date(
-      Number.parseInt(parts[0]),
-      Number.parseInt(parts[1]) - 1,
-      Number.parseInt(parts[2]),
-    ); // Note: months are 0-based
+    return new Date(year, month, day); // Note: months are 0-based
   });
 }
